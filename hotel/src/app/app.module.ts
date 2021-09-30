55631d8b5f57d047b054190d8e2ec659c30a5765
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2021-09-30 14:23:52 -0400

    first commit

--- hotel/src/app/app.module.ts
+++ hotel/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/template/header/header.component';
+import { FooterComponent } from './component/template/footer/footer.component';
+import { SidenavComponent } from './component/template/sidenav/sidenav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
