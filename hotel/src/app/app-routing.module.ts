52f098761d980f035809ee29595f3ec7567c5143
Gabriell Castro de Carvalho <49693579+GabriellCdeC@users.noreply.github.com>
2021-10-04 08:31:43 -0400

    tarefa-9

---
+++ hotel/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/view/home/home.component';
+import { HotelFormComponent } from './component/view/hotel/hotel-form/hotel-form.component';
+import { HotelListComponent } from './component/view/hotel/hotel-list/hotel-list.component';
+import { QuartoFormComponent } from './component/view/hotel/quarto-form/quarto-form.component';
+import { QuartoListComponent } from './component/view/hotel/quarto-list/quarto-list.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'hoteis', component: HotelListComponent },
+  { path: 'hoteis/form', component: HotelFormComponent },
+  { path: 'quartos', component: QuartoListComponent },
+  { path: 'quartos/form', component: QuartoFormComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- hotel/src/app/app.module.ts
+++ hotel/src/app/app.module.ts
@@ -6,17 +6,42 @@ import { HeaderComponent } from './component/template/header/header.component';
 import { FooterComponent } from './component/template/footer/footer.component';
 import { SidenavComponent } from './component/template/sidenav/sidenav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { AppRoutingModule } from './app-routing.module';
+import { HotelListComponent } from './component/view/hotel/hotel-list/hotel-list.component';
+import { HomeComponent } from './component/view/home/home.component';
+import { HotelFormComponent } from './component/view/hotel/hotel-form/hotel-form.component';
+import { QuartoListComponent } from './component/view/hotel/quarto-list/quarto-list.component';
+import { QuartoFormComponent } from './component/view/hotel/quarto-form/quarto-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidenavComponent
+    SidenavComponent,
+    HotelListComponent,
+    HomeComponent,
+    HotelFormComponent,
+    QuartoListComponent,
+    QuartoFormComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
